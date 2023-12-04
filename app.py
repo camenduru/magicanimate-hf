dfd818312e35f6a901554454ff10340d50d15bdd
Apolinário from multimodal AI art <multimodalart@users.noreply.huggingface.co>
2023-12-04 20:27:23 +0000

    fix

--- app.py
+++ app.py
@@ -97,8 +97,7 @@ with gr.Blocks() as demo:
             ["inputs/applications/source_image/multi1_source.png", "inputs/applications/driving/densepose/multi_dancing.mp4"],
         ],
         inputs=[reference_image, motion_sequence],
-        outputs=animation,
-        cache_examples=True
+        outputs=animation
     )
 
 
